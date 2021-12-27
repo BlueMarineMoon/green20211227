b9fdb653000f2dde576f49a696049895e20789df
BlueMarineMoon <minibus2004@naver.com>
2021-12-27 13:13:51 +0900

    오전수업내용

---
+++ src/org/comstudy21/saram/SaramMain.java
@@ -0,0 +1,10 @@
+package org.comstudy21.saram;
+
+
+
+public class SaramMain {
+	public static void main(String[] args) {
+		System.out.println("-------------- 사람 정보 관리 프로그램 ----------------");
+		R.saramConteroller.action();
+	}
+}
---
+++ src/org/comstudy21/saram/view/SaramView.java
@@ -0,0 +1,12 @@
+package org.comstudy21.saram.view;
+
+import java.util.Scanner;
+
+
+public class SaramView {
+	public void display() {
+		public static Scanner scan = new Scanner(System.in);
+		System.out.println("Saram view display");
+		
+	}
+}
